c488f61de10377cebcb6747dff8918660ef35417
TuVoXuan <voxuantu1235@gmail.com>
2023-05-14 16:46:44 +0700

    fix: run.py file

--- run.py
+++ run.py
@@ -4,9 +4,9 @@ if __name__ == "__main__":
     try:
         uvicorn.run(
             reload=False,
-            app="main:app"
-            host='0.0.0.0'
-            port=8000
+            app="main:app",
+            host='0.0.0.0',
+            port=8000,
         )
     except:
         print('Loi khong chay file main')
